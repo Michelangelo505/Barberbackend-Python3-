9221bb3e0cca797635e36bc41f0aac7cdcd64325
Michelangelo <garrys505@gmail.com>
2020-07-29 15:27:20 +0700

    Barberbackend on python 3 with db

--- Barberbackend/barberback/admin.py
+++ Barberbackend/barberback/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(BarberProfile)
+admin.site.register(BarberService)
+admin.site.register(BarberNews)
+admin.site.register(BarberUserSendNews)
+admin.site.register(ServiceTime)
+admin.site.register(BarberMasters)
+admin.site.register(UserOrders)
+admin.site.register(MasterTimeTable)
